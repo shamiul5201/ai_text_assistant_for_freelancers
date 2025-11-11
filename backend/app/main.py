e7612fe3f5ee78fdef6d07620f233b85d0c2bd0a
MD. SHAMIUL ISLAM <68953368+shamiul5201@users.noreply.github.com>
2025-11-11 17:46:19 +0000

    updated main.py and procfile

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from app.routes import generate, web_ui, admin
 from app.routes.auth import auth
 from fastapi.staticfiles import StaticFiles
+from app.database import Base, engine
+from app.models import user_model
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Text Assistant for freelancers")
 
